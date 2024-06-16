090042c1280c283544823da8956fb2f1cdef314c
Gabxiel30 <108111815+GrystanDev@users.noreply.github.com>
2024-06-16 06:42:36 -0500

    (client/auth/register): shadcn Form + zod

--- src/app/client/auth/register/page.jsx
+++ src/app/client/auth/register/page.jsx
@@ -8,20 +8,86 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import Link from "next/link";
 import { useForm } from "react-hook-form";
 
-export function RegisterClientPage() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    watch,
-  } = useForm();
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 
-  const watchEmail = watch("email"); // Observa el campo de email
-  const watchPhone = watch("phone"); // Observa el campo de teléfono
+const formSchema = z
+  .object({
+    dni: z
+      .string({
+        required_error: "El DNI es obligatorio",
+      })
+      .min(1, "El DNI es obligatorio")
+      .regex(/^\d{8}$/, "El DNI debe contener exactamente 8 números"),
+    first_name: z
+      .string({
+        required_error: "El nombre es obligatorio",
+      })
+      .min(1, "El nombre es obligatorio")
+      .max(50, "El nombre no puede tener más de 50 caracteres"),
+    last_name: z
+      .string({
+        required_error: "El apellido es obligatorio",
+      })
+      .min(1, "El apellido es obligatorio")
+      .max(50, "El apellido no puede tener más de 50 caracteres"),
+    address: z
+      .string()
+      .max(50, "La dirección no puede tener más de 50 caracteres")
+      .optional(),
+    email: z
+      .string()
+      .email("El correo electrónico no es válido")
+      .max(50, "El correo no puede tener más de 50 caracteres")
+      .optional(),
+    phone: z
+      .string()
+      .regex(/^\d{9}$/, "El teléfono debe contener exactamente 9 números")
+      .optional(),
+    plain_password: z
+      .string({
+        required_error: "La contraseña es obligatoria",
+      })
+      .min(1, "La contraseña es obligatoria")
+      .max(50, "La contraseña no puede tener más de 50 caracteres"),
+    password_confirmation: z
+      .string({
+        required_error: "La confirmación de contraseña es obligatoria",
+      })
+      .min(1, "La confirmación de contraseña es obligatoria")
+      .max(
+        50,
+        "La confirmación de contraseña no puede tener más de 50 caracteres"
+      ),
+  })
+  .refine((data) => data.email || data.phone, {
+    message: "El correo o el teléfono es obligatorio",
+    path: ["phone"],
+  })
+  .refine((data) => data.email || data.phone, {
+    message: "El correo o el teléfono es obligatorio",
+    path: ["email"],
+  })
+  .refine((data) => data.plain_password === data.password_confirmation, {
+    message: "Las contraseñas no coinciden",
+    path: ["password_confirmation"],
+  });
+
+export function RegisterClientPage() {
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+  });
 
   const onSubmit = async (data) => {
     try {
@@ -39,15 +105,15 @@ export function RegisterClientPage() {
 
       const json = await res.json();
       console.log(json);
-      // Aquí podrías redirigir al usuario o mostrar un mensaje de éxito
+      // Mensaje de éxito
     } catch (error) {
       console.error("Error during registration:", error);
-      // Aquí podrías mostrar un mensaje de error al usuario
+      // Mensaje de error al usuario
     }
   };
 
   return (
-    <Card className="max-w-max mx-auto">
+    <Card className="max-w-max mx-auto min-w-[calc(35vw)]">
       <CardHeader>
         <CardTitle>Regístrate</CardTitle>
         <CardDescription>
@@ -56,220 +122,153 @@ export function RegisterClientPage() {
         </CardDescription>
       </CardHeader>
       <CardContent>
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className="grid gap-4 sm:grid-cols-1 md:grid-cols-2"
-        >
-          <div className="space-y-4">
-            <div>
-              <Label htmlFor="dni">
-                DNI <span className="text-red-500 text-xs">(*)</span>
-              </Label>
-              <Input
-                id="dni"
-                placeholder="00000001"
-                {...register("dni", {
-                  required: {
-                    value: true,
-                    message: "El DNI es obligatorio",
-                  },
-                  pattern: {
-                    value: /^\d{8}$/,
-                    message: "El DNI debe contener exactamente 8 números",
-                  },
-                })}
-              />
-              {errors?.dni?.message && (
-                <span className="text-red-500 text-xs">
-                  {errors.dni.message}
-                </span>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="grid gap-4 sm:grid-cols-1 md:grid-cols-2"
+          >
+            <FormField
+              control={form.control}
+              name="dni"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>
+                    DNI <span className="text-red-500 text-xs">(*)</span>
+                  </FormLabel>
+                  <FormControl>
+                    <Input placeholder="00000001" {...field} />
+                  </FormControl>
+                  <FormMessage className="text-xs font-normal" />
+                </FormItem>
               )}
-            </div>
+            />
 
-            <div>
-              <Label htmlFor="first_name">
-                Nombre <span className="text-red-500 text-xs">(*)</span>
-              </Label>
-              <Input
-                id="first_name"
-                placeholder="Nombre"
-                {...register("first_name", {
-                  required: "El nombre es obligatorio",
-                  maxLength: {
-                    value: 50,
-                    message: "El nombre no puede tener más de 50 caracteres",
-                  },
-                })}
-              />
-              {errors?.first_name?.message && (
-                <span className="text-red-500 text-xs">
-                  {errors.first_name.message}
-                </span>
+            <FormField
+              control={form.control}
+              name="first_name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>
+                    Nombre <span className="text-red-500 text-xs">(*)</span>
+                  </FormLabel>
+                  <FormControl>
+                    <Input placeholder="Nombre" {...field} />
+                  </FormControl>
+                  <FormMessage className="text-xs font-normal" />
+                </FormItem>
               )}
-            </div>
-
-            <div>
-              <Label htmlFor="last_name">
-                Apellidos <span className="text-red-500 text-xs">(*)</span>
-              </Label>
-              <Input
-                id="last_name"
-                placeholder="Apellido"
-                {...register("last_name", {
-                  required: "El apellido es obligatorio",
-                  maxLength: {
-                    value: 50,
-                    message: "El apellido no puede tener más de 50 caracteres",
-                  },
-                })}
-              />
-              {errors?.last_name?.message && (
-                <span className="text-red-500 text-xs">
-                  {errors.last_name.message}
-                </span>
+            />
+            <FormField
+              control={form.control}
+              name="last_name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>
+                    Apellidos <span className="text-red-500 text-xs">(*)</span>
+                  </FormLabel>
+                  <FormControl>
+                    <Input placeholder="Apellido" {...field} />
+                  </FormControl>
+                  <FormMessage className="text-xs font-normal" />
+                </FormItem>
               )}
-            </div>
-
-            <div>
-              <Label htmlFor="address">Dirección</Label>
-              <Input
-                id="address"
-                placeholder="Dirección"
-                {...register("address", {
-                  maxLength: {
-                    value: 50,
-                    message: "La dirección no puede tener más de 50 caracteres",
-                  },
-                })}
-              />
-              {errors?.address?.message && (
-                <span className="text-red-500 text-xs">
-                  {errors.address.message}
-                </span>
+            />
+            <FormField
+              control={form.control}
+              name="address"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Dirección</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Dirección" {...field} />
+                  </FormControl>
+                  <FormMessage className="text-xs font-normal" />
+                </FormItem>
               )}
-            </div>
-          </div>
-
-          <div className="flex flex-col space-y-4">
-            <div>
-              <Label htmlFor="email">Correo</Label>
-              <Input
-                id="email"
-                placeholder="correo@ejemplo.com"
-                type="email"
-                {...register("email", {
-                  pattern: {
-                    value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-                    message: "El correo electrónico no es válido",
-                  },
-                  maxLength: {
-                    value: 50,
-                    message: "El correo no puede tener más de 50 caracteres",
-                  },
-                  validate: {
-                    required: (value) =>
-                      value || watchPhone
-                        ? true
-                        : "El correo o el teléfono es obligatorio",
-                  },
-                })}
-              />
-              {errors?.email?.message && (
-                <span className="text-red-500 text-xs">
-                  {errors.email.message}
-                </span>
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Correo</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="correo@ejemplo.com"
+                      type="email"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage className="text-xs font-normal" />
+                </FormItem>
               )}
-            </div>
-
-            <div>
-              <Label htmlFor="phone">Teléfono</Label>
-              <Input
-                id="phone"
-                placeholder="000000001"
-                {...register("phone", {
-                  pattern: {
-                    value: /^\d{9}$/,
-                    message: "El teléfono debe contener exactamente 9 números",
-                  },
-                  validate: {
-                    required: (value) =>
-                      value || watchEmail
-                        ? true
-                        : "El correo o el teléfono es obligatorio",
-                  },
-                })}
-              />
-              {errors?.phone?.message && (
-                <span className="text-red-500 text-xs">
-                  {errors.phone.message}
-                </span>
+            />
+            <FormField
+              control={form.control}
+              name="phone"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Teléfono</FormLabel>
+                  <FormControl>
+                    <Input placeholder="000000001" {...field} />
+                  </FormControl>
+                  <FormMessage className="text-xs font-normal" />
+                </FormItem>
               )}
-            </div>
-
-            <div>
-              <Label htmlFor="plain_password">
-                Contraseña <span className="text-red-500 text-xs">(*)</span>
-              </Label>
-              <Input
-                id="plain_password"
-                placeholder="***********"
-                type="password"
-                {...register("plain_password", {
-                  required: "La contraseña es obligatoria",
-                  maxLength: {
-                    value: 50,
-                    message:
-                      "La contraseña no puede tener más de 50 caracteres",
-                  },
-                })}
-              />
-              {errors?.plain_password?.message && (
-                <span className="text-red-500 text-xs">
-                  {errors.plain_password.message}
-                </span>
+            />
+            <FormField
+              control={form.control}
+              name="plain_password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>
+                    Contraseña <span className="text-red-500 text-xs">(*)</span>
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="***********"
+                      type="password"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage className="text-xs font-normal" />
+                </FormItem>
               )}
-            </div>
-
-            <div>
-              <Label htmlFor="password_confirmation">
-                Confirmar contraseña{" "}
-                <span className="text-red-500 text-xs">(*)</span>
-              </Label>
-              <Input
-                id="password_confirmation"
-                type="password"
-                placeholder="***********"
-                {...register("password_confirmation", {
-                  required: {
-                    value: true,
-                    message: "La confirmación de contraseña es obligatoria",
-                  },
-                  maxLength: 50,
-                  validate: (value) =>
-                    value === watch("plain_password") ||
-                    "Las contraseñas no coinciden",
-                })}
-              />
-              {errors?.password_confirmation?.message && (
-                <span className="text-red-500 text-xs">
-                  {errors.password_confirmation.message}
-                </span>
+            />
+            <FormField
+              control={form.control}
+              name="password_confirmation"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>
+                    Confirmar contraseña{" "}
+                    <span className="text-red-500 text-xs">(*)</span>
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="***********"
+                      type="password"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage className="text-xs font-normal" />
+                </FormItem>
               )}
-            </div>
-          </div>
+            />
 
-          <div className="col-span-1 md:col-span-2 flex flex-col space-y-4 items-center">
-            <CardDescription>
-              Ya tienes una cuenta?{" "}
-              <Link href="/client/auth/login" className="text-blue-500">
-                Inicia sesión
-              </Link>
-            </CardDescription>
-            <Button className="w-full" type="submit">
-              Registrar
-            </Button>
-          </div>
-        </form>
+            <div className="col-span-1 md:col-span-2 flex flex-col space-y-4 items-center">
+              <CardDescription>
+                Ya tienes una cuenta?{" "}
+                <Link href="/client/auth/login" className="text-blue-500">
+                  Inicia sesión
+                </Link>
+              </CardDescription>
+              <Button className="w-full" type="submit">
+                Registrar
+              </Button>
+            </div>
+          </form>
+        </Form>
       </CardContent>
     </Card>
   );
