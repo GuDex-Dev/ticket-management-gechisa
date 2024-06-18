6d61d024930f1a6de7477cd11170965d51be53f6
Gabxiel30 <108111815+GrystanDev@users.noreply.github.com>
2024-06-18 07:23:27 -0500

    Add typography + ROLES -> utils

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,8 @@ import { ROLES } from "@/lib/utils";
 import { signOut, useSession } from "next-auth/react";
 import { Button } from "./ui/button";
 import { useAppContext } from "./context/AppSessionContextProvider";
+import { H1 } from "./ui/typography";
+import { useTheme } from "next-themes";
 
 function Navbar({ role }) {
   const { data } = useSession();
@@ -16,22 +18,32 @@ function Navbar({ role }) {
     return "Rol no asignado";
   }
 
+  const navigationMenuTriggerStyle = cva(
+    `group inline-flex h-16 w-max items-center justify-center rounded-none bg-primary text-primary-foreground px-4 py-2 transition-colors ${
+      useTheme().theme.includes("yellow")
+        ? "hover:text-sky-700"
+        : "hover:text-yellow-400"
+    } disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50`
+  );
   return (
     <header className="bg-primary shadow-md">
       <nav className="container h-16 mx-auto flex items-center text-lg font-bold ">
-        <Link
-          href={`/${role.toLowerCase()}/dashboard`}
-          className={cn(navigationMenuTriggerStyle(), "text-3xl")}
-        >
-          GECHISA
-          {role === ROLES.ADMINISTRATOR
-            ? " - Admin"
-            : role === ROLES.CLIENT
-            ? ""
-            : role === ROLES.SALESPERSON
-            ? " - Vendedor"
-            : ""}
-        </Link>
+        <H1>
+          <Link
+            href={`/${role.toLowerCase()}/dashboard`}
+            className={cn(navigationMenuTriggerStyle(), "text-4xl -ml-10")}
+          >
+            GECHISA
+            {role === ROLES.ADMINISTRATOR
+              ? " - ADMINISTRADOR"
+              : role === ROLES.CLIENT
+              ? ""
+              : role === ROLES.SALESPERSON
+              ? " - VENDEDOR"
+              : ""}
+          </Link>
+        </H1>
+
         <div className="flex ml-auto items-center">
           {data?.user?.role !== role ? (
             <>
@@ -90,9 +102,5 @@ function Navbar({ role }) {
     </header>
   );
 }
-
-export const navigationMenuTriggerStyle = cva(
-  "group inline-flex h-16 w-max items-center justify-center rounded-none bg-primary text-primary-foreground px-4 py-2 transition-colors hover:bg-accent hover:text-accent-foreground disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50"
-);
 
 export default Navbar;
---
+++ src/components/ui/typography.jsx
@@ -0,0 +1,7 @@
+export function H1({ children }) {
+  return (
+    <h1 className="scroll-m-20 text-4xl font-bold tracking-tight lg:text-4xl">
+      {children}
+    </h1>
+  );
+}
